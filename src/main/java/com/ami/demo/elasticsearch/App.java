2ad8e819f80d47a9c9e9d3f672f7e8101d9d4078
Ami.Tsai <ami@ami.com>
2020-05-18 19:09:53 +0800

    增加spring web pom

--- src/main/java/com/ami/demo/elasticsearch/App.java
+++ src/main/java/com/ami/demo/elasticsearch/App.java
@@ -3,8 +3,8 @@ package com.ami.demo.elasticsearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 //java 與elasticsearch交互範例
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args){
